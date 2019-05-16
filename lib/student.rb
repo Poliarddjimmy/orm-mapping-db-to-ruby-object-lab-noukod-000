6fff4c25caf4dfd98eb5c2295a5f887b4dc0155e
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-05-16 18:30:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,7 +62,7 @@ class Student
     end.first
   end
   
-  def self.all_students_in_grade_9(grade)
+  def self.all_students_in_grade_9(grade=nil)
     sql = <<-SQL
       SELECT *
       FROM students
