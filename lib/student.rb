ad142ddff1ef6cc1455fdece05d376ab909b3403
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-05-16 18:29:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -67,12 +67,11 @@ class Student
       SELECT *
       FROM students
       WHERE grade = ?
-      LIMIT 1
     SQL
  
     DB[:conn].execute(sql, grade).map do |row|
       self.new_from_db(row)
-    end.first
+    end
   end
 
 
